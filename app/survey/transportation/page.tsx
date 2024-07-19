89afdba2ef57b18a20670d4fcd6c37a22c39f6b9
Daniel Santos <skyglis.suporte@gmail.com>
2024-07-19 19:12:10 +0100

    Prod Fix

--- app/survey/transportation/page.tsx
+++ app/survey/transportation/page.tsx
@@ -111,6 +111,7 @@ export default function TransportationPage() {
                 { s.option }
               </span>
               <input
+              key={ s.option }
               type="number"
               placeholder="30"
               className={clsx(
