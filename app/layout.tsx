e782f7396edc7d1f1b3523175d0d46b202d7a1dc
Antonio Carvalho <antonio.carvalho@origamihub.tech>
2024-07-22 10:37:05 +0100

    Nova imagem de fundo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import {NextUIProvider} from "@nextui-org/react";
 import siteBackGround1 from '@/images/arrows-8841234_1280.jpg';
 import siteBackGround2 from '@/images/tree-5725540_1280.jpg';
 import siteBackGround3 from '@/images/sustainability-8438275_1280.png';
+import siteBackGround4 from '@/images/bg-cooler-world.svg';
 
 import {Providers} from "./providers";
 
@@ -33,7 +34,7 @@ export default function RootLayout({
   return (
       <html lang="en" className="h-full">
         <body className="lg:bg-faint-green font-ubuntu h-full flex flex-col justify-start lg:justify-center items-center">
-          <Image src={siteBackGround3} alt="" className="w-[250px] lg:h-auto backgroundImage" fill style={{objectFit: 'cover',}}/>
+          <Image src={siteBackGround4} alt="" className="w-[250px] lg:h-auto backgroundImage" fill style={{objectFit: 'cover',}}/>
           <Providers>
             <main className="font-normal relative w-full max-w-lg lg:max-w-[940px]">
               {children}
