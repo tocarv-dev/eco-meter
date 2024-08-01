022b640dcafe135683fd9e9862891aebdfce0e88
Antonio Carvalho <antonio.carvalho@origamihub.tech>
2024-08-01 17:39:46 +0100

    - alteração da base de dados para postgres - autenticação com GitHub

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,2 @@
 import { handlers } from "@/lib/auth/auth" // Referring to the auth.ts we just created
 export const { GET, POST } = handlers
-export const runtime = "edge" // optional
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import Resend from "next-auth/providers/resend"
 
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
@@ -10,11 +9,6 @@ const prisma = new PrismaClient()
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub,
-    Resend({
-      // If your environment variable is named differently than default
-      apiKey: process.env.AUTH_RESEND_KEY,
-      from: "no-reply@origamihub.tech"
-    }),
+    GitHub
   ],
 })
