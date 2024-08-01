8b387fb936a8bf22407d461a2924d8f1cbac5936
Antonio Carvalho <antonio.carvalho@origamihub.tech>
2024-08-01 17:42:26 +0100

    cleanup migration files

--- prisma/migrations/20240729132915_mig3/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `surveys` MODIFY `bags_unsorted` INTEGER NULL,
-    MODIFY `bags_paper` INTEGER NULL,
-    MODIFY `bags_plastic` INTEGER NULL,
-    MODIFY `bags_glass` INTEGER NULL,
-    MODIFY `bags_organic` INTEGER NULL;
